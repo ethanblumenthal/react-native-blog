f247fcd0697906c9747ba6300ad3fcdde5ceb7bb
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-12 16:42:44 -0500

    refactored to use useReducer for blog CRUD functionality

---
+++ src/context/createDataContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useReducer } from 'react';
+
+export default (reducer, actions, initalState) => {
+  const Context = createContext();
+
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initalState);
+
+    const boundActions = {};
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
+
+    return <Context.Provider value={{ state, ...boundActions }}>{children}</Context.Provider>;
+  };
+
+  return { Context, Provider };
+};
