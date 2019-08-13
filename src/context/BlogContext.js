b2ef348579a2b8b765e17838cc3308bb193b9bea
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-13 10:00:22 -0500

    implemented network requests for create, edit, and delete blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,15 +5,6 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case 'get_blogposts':
       return action.payload;
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case 'edit_blogpost':
       return state.map(blogPost => {
         return blogPost.id === action.payload.id ? action.payload : blogpost;
@@ -33,21 +24,23 @@ const getBlogPosts = dispatch => {
 };
 
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
     if (callback) callback();
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
     if (callback) callback();
   };
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
