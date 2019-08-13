40f3ca491ca532ca4a5a48c16bc30ae659ba94ba
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-13 09:52:24 -0500

    implemented data fetching from remote server

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+      return action.payload;
     case 'add_blogpost':
       return [
         ...state,
@@ -20,6 +23,13 @@ const blogReducer = (state, action) => {
     default:
       return state;
   }
+};
+
+const getBlogPosts = dispatch => {
+  return async () => {
+    const { data } = await jsonServer.get('/blogposts');
+    dispatch({ type: 'get_blogposts', payload: data });
+  };
 };
 
 const addBlogPost = dispatch => {
@@ -44,6 +54,6 @@ const deleteBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: 'Test Post', content: 'Test Content', id: 1 }],
+  { getBlogPosts, addBlogPost, editBlogPost, deleteBlogPost },
+  [],
 );
